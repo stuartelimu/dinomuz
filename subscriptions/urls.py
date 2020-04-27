0c60b9682b9da8b86745289a99cd79d7f193a9f2
ElimuStuart <stuartelimu@gmail.com>
2020-04-27 08:59:14 +0300

    modify index view

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from .views import IndexView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', IndexView.as_view(), name='index'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Subscriber
+from .forms import SubscriberForm
+
+
+class IndexView(View):
+    form_class = SubscriberForm
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})     
