d7ea606329f4102cd8f97bbe9c7ea55ee39b536f
ElimuStuart <stuartelimu@gmail.com>
2020-04-27 08:27:35 +0300

    create models for subscriptions

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
